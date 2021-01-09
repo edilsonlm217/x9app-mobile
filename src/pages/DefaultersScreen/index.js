a88ba670f4685931d5090fb3dd9b2c92c7dbcb53
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-09 13:00:44 -0400

    Implementação da tela de alunos inadiplentes

---
+++ src/pages/DefaultersScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function DefaultersScreen() {
+  return (
+    <View>
+      <Text>DefaultersScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
