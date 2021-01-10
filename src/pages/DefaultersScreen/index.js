3721710adee39768c5b0a86b2675edfdce0c3578
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-10 12:59:58 -0400

    Implementação do header

--- src/pages/DefaultersScreen/index.js
+++ src/pages/DefaultersScreen/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
 export default function DefaultersScreen() {
+  const windowHeight = Dimensions.get('window').height * 0.19;
+
   return (
-    <View>
-      <Text>DefaultersScreen</Text>
+    <View style={styles.page_container}>
+      <View style={[styles.page_header, { height: windowHeight }]}>
+        <Text style={styles.header_label}>Alunos em atraso</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  page_container: {
+    flex: 1,
+    backgroundColor: '#F7F7F7',
+  },
 
+  page_header: {
+    backgroundColor: '#F43C08',
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 10,
+  },
+
+  header_label: {
+    color: '#FFFFFF',
+    fontFamily: 'Roboto-Bold',
+    fontSize: 18,
+    zIndex: 1,
+  },
 });
