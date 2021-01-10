27c4eb659ddadfb91cb87ae3a3e4cb0d65f03969
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-09 23:49:46 -0400

    Implementação para listagem de todos os alunos

--- src/pages/SearchScreen/index.js
+++ src/pages/SearchScreen/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions, Alert, FlatList } from 'react-native';
 import MIcon from 'react-native-vector-icons/MaterialIcons';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import api from '../../services/api';
 
 export default function SearchScreen() {
   const windowHeight = Dimensions.get('window').height * 0.19;
+
+  const [searchResult, setSearchResult] = useState([]);
+  const [noContent, setNoContent] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  async function fetchClients() {
+    try {
+      setLoading(true);
+
+      const response = await api.get('clients/all');
+
+      setSearchResult(response.data);
+      setLoading(false);
+    } catch {
+      setLoading(false);
+      Alert.alert('Erro', 'Não foi possível carregar seus alunos');
+    }
+  }
+
+  useEffect(() => {
+    fetchClients();
+  }, []);
 
   return (
     <View style={styles.page_container}>
@@ -21,33 +44,23 @@ export default function SearchScreen() {
         </View>
       </View>
 
-      <View
-        style={{
-          backgroundColor: '#F7F7F7',
-          flex: 1,
-          paddingTop: 30,
-        }}
-      >
-        <View style={{
-          borderRadius: 5,
-          borderColor: '#707070',
-          borderWidth: StyleSheet.hairlineWidth,
-          marginLeft: 15,
-          marginRight: 15,
-        }}>
-          <TouchableOpacity
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              padding: 10,
-              borderRadius: 5,
-              backgroundColor: '#FFFFFF',
-            }}
-          >
-            <Text>Edilson Rocha Lima</Text>
-            <MIcon name="chevron-right" size={22} color="#222222" />
-          </TouchableOpacity>
+      <View style={styles.section_container}>
+        <View style={styles.flatlist_container} >
+          <FlatList
+            data={searchResult}
+            keyExtractor={item => item.id}
+            ItemSeparatorComponent={() => (
+              <View style={styles.separator_style} />
+            )}
+            renderItem={({ item }) => (
+              <TouchableOpacity style={styles.search_result_btn}>
+                <Text>{item.name}</Text>
+                <MIcon name="chevron-right" size={22} color="#222222" />
+              </TouchableOpacity>
+            )}
+            refreshing={loading}
+            onRefresh={() => fetchClients()}
+          />
         </View>
 
       </View>
@@ -113,5 +126,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 5,
+  },
+  search_result_btn: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+  },
+
+  separator_style: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#CBCBCB'
+  },
+
+  flatlist_container: {
+    borderRadius: 5,
+    borderColor: '#707070',
+    borderWidth: StyleSheet.hairlineWidth,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+
+  section_container: {
+    backgroundColor: '#F7F7F7',
+    flex: 1,
+    paddingTop: 30,
   },
 });
