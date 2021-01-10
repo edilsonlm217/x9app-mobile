749e470082946417d950f656685a9316e691573b
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-10 00:56:19 -0400

    Criação da view de detalhes do cliente

---
+++ src/pages/ClientDetails/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ClientDetails() {
+  return (
+    <View>
+      <Text>ClientDetails</Text>
+    </View>
+  );
+}
